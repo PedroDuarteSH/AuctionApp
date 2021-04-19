ba210edd2e2a22391185bc3986aab4e1d617ff4c
Pedro Henriques <pedroduartesh@gmail.com>
2021-04-19 18:00:00 +0100

    END

--- docker-compose.sh
+++ docker-compose.sh
@@ -18,4 +18,4 @@
 mkdir -p python/app/logs
 
 # add  -d  to the command below if you want the containers running in background without logs
-docker-compose  -f docker-compose-python-psql.yml up --build
+docker-compose  -f docker-compose.yml up --build
